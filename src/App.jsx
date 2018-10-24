c5fd691309bf20f9778a44d943b98094aa7a9998
Samuel Warren <43582445+sWarren35@users.noreply.github.com>
2018-10-23 18:41:38 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-
+import Userinput from "./userinput"
+import Todo from "./todo"
 class App extends Component {
-
-  render() {
-    return (
-      <div className='container'>
-
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      text: '',
+      priority: 1,
+      todos:[]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleDelete= this.handleDelete.bind(this);
   }
-}
 
+
+ handleChange(e)  {   
+    this.setState
+    ({text:e.target.value})
+    //console.log(this.state.text)
+  }
+  handleSelectChange(e) {
+    this.setState
+    ({priority: e.target.value})
+  }
+   
+  handleSubmit(event) {
+    //console.log(event);
+    this.setState({
+      id: this.state.id + 1,
+      text: '',
+      priority: '',
+      todos: [...this.state.todos, {id:this.state.id, text:this.state.text, priority: this.state.priority}]
+    });
+    event.preventDefault();
+    //console.log(this.state);
+  }
+  handleDelete(){
+    console.log();
+
+  }
+ 
+
+render() {
+      return(
+        <div>
+       <Userinput 
+       value={this.state.text} 
+       onChange={this.handleChange} 
+       onSubmit={this.handleSubmit}
+       onChangeSelect={this.handleSelectChange}
+       />
+       <Todo 
+       todos={this.state.todos}
+       onDelete={this.handleDelete()}
+
+       />
+      </div>
+      );
+    }
+  }
+
+  
 export default App;
+
